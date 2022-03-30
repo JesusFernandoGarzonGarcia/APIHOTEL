432fcf63e39163faecb8ecac27be24b73b9db3c3
Jesus Garzon <jesus.garzon@uptc.edu.co>
2022-03-30 00:08:28 -0500

    primera actualizacion

---
+++ routes.js
@@ -0,0 +1,55 @@
+const express = require('express')
+
+const routes = express.Router()
+
+routes.get('/',(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM userregistration',(err,rows)=>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+routes.post('/',(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('INSERT INTO userregistration SET ?',[req.body],(err,rows)=>{
+           if(err) return res.send(err)
+
+         res.json('usuario registrado')
+        })
+    })
+})
+
+routes.delete('/:id',(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('DELETE FROM userregistration WHERE id=?',[req.params.id],(err,rows)=>{
+           if(err) return res.send(err)
+
+         res.json('usuario Eliminado')
+        })
+    })
+})
+
+routes.put('/:id',(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('UPDATE userregistration set ? WHERE id=?',[req.body,req.params.id],(err,rows)=>{
+           if(err) return res.send(err)
+
+         res.json('Usuario Actualizado')
+        })
+    })
+})
+
+
+
+module.exports = routes
---
+++ server.js
@@ -0,0 +1,35 @@
+const express =require('express')
+const mysql= require('mysql')
+const myconn = require('express-myconnection')
+
+const routes = require('./routes')
+const { json } = require('express/lib/response')
+//const res = require('express/lib/response')
+
+const app =express()
+app.set('port',process.env.PORT ||9000)
+
+
+const dboptions ={
+host: 'bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com',
+port: '3306',
+user:'u5kqcja6qbfwixja',
+password:'vzKTAoGQctvrmZ3BuEoG',
+database:'bhxcvvnq9j6lo7aynk87'
+}
+
+// middelware-------------------------
+app.use(myconn(mysql,dboptions,'single'))
+app.use(express.json())
+
+//Routes------------------------------
+app.get('/',(req,res)=>{
+    res.send('Welcom to my API')
+})
+
+app.use('/api',routes)
+
+// server runing ---------------------
+app.listen(app.get('port'),()=>{
+    console.log('server runing on server ',app.get('port'))
+})
