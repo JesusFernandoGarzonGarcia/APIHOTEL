01d49140f76e373ebb9347f173f5a72f21ab52d9
JesusFernandoGarzon <jesus.garzon@uptc.edu.co>
2022-03-31 05:53:44 -0500

    ghjk

--- routes.js
+++ routes.js
@@ -103,7 +103,7 @@ routes.post('/',(req,res)=>{
     })
 })
 
-routes.get('/accessRoket',(req,res)=>{
+routes.post('/accessRoket',(req,res)=>{
     res.preventDefault()
     ax({
         method: 'POST',
