6f1602716f38f89bfc5a21e79ef0903656f4da46
Jesus Fernando Garzon <jesus.garzon@uptc.edu.co>
2022-03-30 20:42:22 -0500

    add jwt my api

--- funtions.js
+++ funtions.js
@@ -5,3 +5,17 @@ require('dotenv').config()
 function generateAccessToken(user){
 return jwt.sign(user,process.env.SECRET,{expirenIn:'5m'})
 }
+
+
+function validateToken(req, res,next){
+    const accessToken = req.header['authorization'] || req.query.accessToken
+    if(!accessToken) res.send('Access denied')
+    
+    jwt.verify(accessToken,process.env.SECRET,(err,user)=>{
+        if(err){
+            res.send('access denied, token expired or incorrect')
+        }else{
+            next()
+        }
+    })
+    }
